f79713e1def89af52e562f3f4b9eb0276c32c59a
Tom Hancock <thomas.ian.hancock@gmail.com>
2013-12-14 19:08:22 +0000

    Fix up tests

--- test/zeitgeist/markov_test.clj
+++ test/zeitgeist/markov_test.clj
@@ -1,15 +1,15 @@
-(ns zeitgeist.core-test
+(ns zeitgeist.markov-test
   (:require [clojure.test :refer :all]
             [zeitgeist.markov :as markov :refer :all]))
 
 (deftest tokenisation
   (testing "Basic tokenisation"
-    (is (= (markov/tokenise "Hello there") ["Hello" "there"])))
+    (is (= (markov/tokenise "Hello there") '("hello" "there"))))
   (testing "With all the trimmings"
     (is (= (markov/tokenise "Hello. there , I?!?do(n't bel)ieve we've m:et")
-           ["Hello" "there" "I" "do" "n't" "bel" "ieve" "we've" "m" "et"]))))
+           '("hello" "there" "i" "do" "n't" "bel" "ieve" "we've" "m" "et")))))
 
 (deftest bigramisation
   (testing "Bigram generation"
     (is (= (markov/bigrams "Hello there, how are you?")
-           '(("Hello" "there") ("there" "how") ("how" "are") ("are" "you"))))))
+           '(("hello" "there") ("there" "how") ("how" "are") ("are" "you"))))))
