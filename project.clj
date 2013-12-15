5862dd9bad5352e51a76c1aad5e7cf3198b6d730
Tom Hancock <thomas.ian.hancock@gmail.com>
2013-12-15 17:59:02 +0000

    Output gibberish "zeitgeist"

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :main zeitgeist.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http            "0.7.8"]
+                 [io.curtis/boilerpipe-clj "0.3.0"]])
--- src/zeitgeist/core.clj
+++ src/zeitgeist/core.clj
@@ -1,6 +1,12 @@
-(ns zeitgeist.core)
+(ns zeitgeist.core
+  (:require [zeitgeist.scraper :as scraper]
+            [zeitgeist.markov :as markov]
+            [clojure.string :as string]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  []
+  (let [scrapings (scraper/scrape)
+        m (markov/markov-map (apply str scrapings))
+        map-keys (keys m)
+        initial (nth map-keys (rand-int (count map-keys)))]
+    (println (string/join " " (take 100 (markov/generate-chain m initial))))))
